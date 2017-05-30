4cf3eb03dfa646a7bcd66a81c5f004833c6c4435
Brandon Smith <Brandon Smith>
2017-05-30 17:21:28 -0600

    set up navbar

---
+++ src/components/mainNav.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class MainNav extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-inverse sticky-top">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <Link to="/" className="navbar-left"><img className="navbarLogo" src="./src/imgs/soundWaveLogo.png" /></Link>
+                    </div>
+                    <ul className="nav navbar-nav">
+                        <li>
+                            <Link to="/">
+                                <h4 className="navbarText active">Home</h4>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/test">
+                                <h4 className="navbarText">Genres</h4>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/test">
+                                <h4 className="navbarText">Producers</h4>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/test">
+                                <h4 className="navbarText">Artists</h4>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/test">
+                                <h4 className="navbarText">Top Tracks</h4>
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><Link to="/"><div id="profileIconDiv" className="navbarProfileIcon pulse"></div></Link></li>
+                        <li><Link to="/"><div id="gearIconDiv" className="navbarProfileIcon"></div></Link></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default MainNav;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import promise from 'redux-promise';
+import $ from 'jQuery';
+
+import reducers from './reducers';
+import MainNav from './components/mainNav';
+import Home from './components/homeContainer';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+class Test extends Component {
+    render() {
+        return <div className="container">TEST!</div>
+    } 
+}
+ 
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <MainNav />
+                <Switch>
+                    <Route path='/test' component={Test} />
+                    <Route path='/' component={Home} />
+                </Switch>
+            </div> 
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.reactContainer'));
