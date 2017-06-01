f02d31d12337396500f1f94ee7b1669979886328
Brandon Smith <Brandon Smith>
2017-06-01 09:53:42 -0600

    finished styling home page

--- src/components/homeContainer.js
+++ src/components/homeContainer.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
-
+import {Link} from 'react-router-dom';
 
 
 class Home extends Component {
     render() {
         return (
-            <div className="container">
-                <h1>HomePage!</h1>
+            <div className="container-fluid">
+                <div id="homeColumnRow" className="row">
+                    <div className="col-sm-4 homeColumn">
+                        <Link to="/test" className="grow">
+                            <div id="homeSearchIcon" className="homeColumnImage"></div>
+                            <h3 className="homeColumnText">Find a Track</h3>
+                        </Link>
+                    </div>
+                    <div className="col-sm-4 homeColumn">
+                        <Link to="/test" className="grow">
+                            <div id="homeCollaborationIcon" className="homeColumnImage"></div>
+                            <h3 className="homeColumnText">Start a Collaboration</h3>
+                        </Link>
+                    </div>
+                    <div className="col-sm-4 homeColumn">
+                        <Link to="/test" className="grow">
+                            <div id="homeUploadTrackIcon" className="homeColumnImage"></div>
+                            <h3 className="homeColumnText">Upload a Track</h3>
+                        </Link>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/mainNav.js
+++ src/components/mainNav.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom';
 class MainNav extends Component {
     render() {
         return (
-            <nav className="navbar navbar-inverse sticky-top">
+            <nav id="myNav" className="navbar navbar-inverse sticky-top">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
                             <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
@@ -15,37 +15,39 @@ class MainNav extends Component {
                         </button>
                         <Link to="/" className="navbar-left"><img className="navbarLogo" src="./src/imgs/soundWaveLogo.png" /></Link>
                     </div>
-                    <ul className="nav navbar-nav">
-                        <li>
-                            <Link to="/">
-                                <h4 className="navbarText active">Home</h4>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/test">
-                                <h4 className="navbarText">Genres</h4>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/test">
-                                <h4 className="navbarText">Producers</h4>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/test">
-                                <h4 className="navbarText">Artists</h4>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/test">
-                                <h4 className="navbarText">Top Tracks</h4>
-                            </Link>
-                        </li>
-                    </ul>
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><Link to="/"><div id="profileIconDiv" className="navbarProfileIcon pulse"></div></Link></li>
-                        <li><Link to="/"><div id="gearIconDiv" className="navbarProfileIcon"></div></Link></li>
-                    </ul>
+                    <div className="collapse navbar-collapse" id="navbar1">
+                        <ul className="nav navbar-nav">
+                            <li>
+                                <Link to="/">
+                                    <h4 className="navbarText active">Home</h4>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/test">
+                                    <h4 className="navbarText">Genres</h4>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/test">
+                                    <h4 className="navbarText">Producers</h4>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/test">
+                                    <h4 className="navbarText">Artists</h4>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/test">
+                                    <h4 className="navbarText">Top Tracks</h4>
+                                </Link>
+                            </li>
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><Link to="/"><div id="profileIconDiv" className="navbarProfileIcon pulse"></div></Link></li>
+                            <li><Link to="/"><div id="gearIconDiv" className="navbarProfileIcon"></div></Link></li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         );
